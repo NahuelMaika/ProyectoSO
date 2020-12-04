31b2db9fe0a4f6e1328832520ef337ffd5d845a8
Nahuel Maika <nahuelmaikagmail.com>
2020-12-03 21:21:09 -0300

    Corregido comidarapida con colas de mensajes

--- ComidaRapida3.c
+++ ComidaRapida3.c
@@ -11,14 +11,25 @@
 #include "constComidaRapida.h"
 #include <time.h>
 
-#define M 30	//Mesas
-#define N 50	//Clientes
-#define C 3
+#define M 30	//Mesas 30
+#define N 50	//Clientes 50
+#define C 3 	//Cocineros
+#define K 10    //Espacio en la cola de comida
 
-void * comer(void * z){ 
-	int * aux = (int*)z;
-	tMessage recibo, respuesta;
+int abrirColaClientes(){
+	key_t key = ftok(FILE_PATH, 1);
+	if (key < 0) {
+		report_and_exit("ftok");
+	}
+	int queueId = msgget(key, 0666 | IPC_CREAT);
+	if (queueId < 0) {
+		report_and_exit("msgget");
+	}
+	
+	return queueId;
+}
 
+int abrirColaComidas(){
 	key_t key = ftok(FILE_PATH, 2);
 	if (key < 0) {
 		report_and_exit("ftok");
@@ -27,27 +38,57 @@ void * comer(void * z){
 	if (queueId < 0) {
 		report_and_exit("msgget");
 	}
+	
+	return queueId;
+}
 
-	msgrcv(queueId, &recibo, SIZE_MSG, TYPE_1, 0); // espero por mesa disponible
+int abrirColaCamarero(){
+	key_t key = ftok(FILE_PATH, 3);
+	if (key < 0) {
+		report_and_exit("ftok");
+	}
+	int queueId = msgget(key, 0666 | IPC_CREAT);
+	if (queueId < 0) {
+		report_and_exit("msgget");
+	}
+	
+	return queueId;
+}
+
+void * comer(void * z){ 
+	int * aux = (int*)z;
+	tMessage recibo, respuesta;
+
+	int queue_cliente_Id, queue_camarero_Id;
+	queue_cliente_Id=abrirColaClientes();
+	queue_camarero_Id=abrirColaCamarero();
+
+	msgrcv(queue_cliente_Id, &recibo, SIZE_MSG, Tipo_Mesas, 0); // espero por mesa disponible
 	printf("\nSoy el Cliente nro: %i y me sente en una mesa\n", (*aux)+1);
 	
 	if(rand()%2){
-		strcpy(respuesta.body, "Vegetariano");
+		respuesta.type = Tipo_Vegetal;
+		msgsnd(queue_camarero_Id, &respuesta, SIZE_MSG, IPC_NOWAIT);
+		printf("Soy cliente nro %i y pedi un plato vegetal\n", (*aux)+1);
+		
+		msgrcv(queue_cliente_Id, &recibo, SIZE_MSG, Tipo_Vegetal, 0);//espero por mi pedido vegetal
+		
+		printf("Soy cliente nro %i me dieron mi plato vegetal y voy a comer\n", (*aux)+1);
+		
 	}else{
-		strcpy(respuesta.body, "Carnivoro");
+		respuesta.type = Tipo_Carne;
+		msgsnd(queue_camarero_Id, &respuesta, SIZE_MSG, IPC_NOWAIT);
+		printf("Soy cliente nro %i y pedi un plato carne\n", (*aux)+1);
+		
+		msgrcv(queue_cliente_Id, &recibo, SIZE_MSG, Tipo_Carne, 0);//espero por mi pedido carne
+		
+		printf("Soy cliente nro %i me dieron mi plato carne y voy a comer\n", (*aux)+1);
 	}
-	
-	respuesta.type = TYPE_2;
-	msgsnd(queueId, &respuesta, SIZE_MSG, IPC_NOWAIT);
-	printf("Soy cliente nro %i y pedi un plato %s\n", (*aux)+1, respuesta.body);
-	
-	msgrcv(queueId, &recibo, SIZE_MSG, TYPE_3, 0); // espero por mi pedido 
-	
-	printf("Soy cliente nro %i me dieron mi plato %s y voy a comer\n", (*aux)+1, recibo.body); // primero el camarero le tiene que entregar el plato?
+
 	printf("Soy cliente nro %i y me voy\n", (*aux)+1);
 	
-	respuesta.type = TYPE_4;
-	msgsnd(queueId, &respuesta, SIZE_MSG, IPC_NOWAIT);
+	respuesta.type = Tipo_Limpiador;
+	msgsnd(queue_cliente_Id, &respuesta, SIZE_MSG, IPC_NOWAIT);
 	
 	pthread_exit(0);
 }
@@ -56,26 +97,19 @@ void * cocinar(void * z){
 	int * aux = (int*)z;
 
 	tMessage recibo, respuesta;
-
-	key_t key = ftok(FILE_PATH, 2);
-	if (key < 0) {
-		report_and_exit("ftok");
-	}
-	int queueId = msgget(key, 0666 | IPC_CREAT);
-	if (queueId < 0) {
-		report_and_exit("msgget");
-	}
+	int queue_comidas_Id = abrirColaComidas();
 	
-	// resto un lugar en el semaforo cola = pongo un nuevo plato de comida
 	while(1){
-		msgrcv(queueId, &recibo, SIZE_MSG, TYPE_5, 0); // espero por pedido de plato carnivoro o vegetariano
-		
-		printf("\nSoy cocinero %i y estoy cocinando.......entrego un plato de comida\n", (*aux));
-		sleep(1);
-		
-		respuesta.type = TYPE_6;
-		strcpy(respuesta.body, recibo.body);
-		msgsnd(queueId, &respuesta, SIZE_MSG, IPC_NOWAIT);
+		msgrcv(queue_comidas_Id, &recibo, SIZE_MSG, Tipo_Cola_Comida, 0); // espero a que haya lugar en la cola para cocinar
+
+		if(rand()%2){
+			respuesta.type = Tipo_Vegetal;
+			printf("\nSoy cocinero %i y estoy cocinando un plato vegetal \n", (*aux));
+		}else{
+			respuesta.type = Tipo_Carne;
+			printf("\nSoy cocinero %i y estoy cocinando un plato carne\n", (*aux));
+		}
+		msgsnd(queue_comidas_Id, &respuesta, SIZE_MSG, IPC_NOWAIT);
 	}		
 	pthread_exit(0);
 }
@@ -83,30 +117,33 @@ void * cocinar(void * z){
 /*
  * El camarero toma la comida de la cola y la entrega a un cliente que ha pedido una comida
  */
-void * entrgarComida(void * arg){
+void * entregarComida(void * arg){
 	
 	tMessage recibo, respuesta;
-
-	key_t key = ftok(FILE_PATH, 2);
-	if (key < 0) {	report_and_exit("ftok");	}
-	int queueId = msgget(key, 0666 | IPC_CREAT);
-	if (queueId < 0) {	report_and_exit("msgget");	}
 	
+	int queue_cliente_Id,queue_comidas_Id,queue_camarero_Id;
+	queue_cliente_Id =abrirColaClientes();
+	queue_comidas_Id = abrirColaComidas();
+	queue_camarero_Id = abrirColaCamarero();
+	printf("SOY CAMARERO\n");
 	while(1){
-		msgrcv(queueId, &recibo, SIZE_MSG, TYPE_2, 0); // espero por pedido de plato carnivoro o vegetariano
-
-		printf("Soy camarero y ordene al chef hacer un plato\n");
-		respuesta.type = TYPE_5; // Se ordena al chef que haga un plato
-		strcpy(respuesta.body, recibo.body);
-		msgsnd(queueId, &respuesta, SIZE_MSG, IPC_NOWAIT);
 		
+		msgrcv(queue_camarero_Id, &recibo, SIZE_MSG,0,0); //Hay un pedido de vegetal
+		if(recibo.type == Tipo_Vegetal){
+			printf("Soy el camarero y me pidieron un plato vegetal\n");
+			msgrcv(queue_comidas_Id, &recibo, SIZE_MSG, Tipo_Vegetal, 0);
+			printf("Soy el camarero y tome un plato de vegetal\n");
+		}
+		if(recibo.type == Tipo_Carne){ //Hay un pedido de carne
+			printf("Soy el camarero y me pidieron un plato carne\n");
+			msgrcv(queue_comidas_Id, &recibo, SIZE_MSG, Tipo_Carne, 0);
+			printf("Soy el camarero y tome un plato de carne\n");
+		}
 		
-		msgrcv(queueId, &recibo, SIZE_MSG, TYPE_6, 0); // espero por pedido de plato carnivoro o vegetariano
-		printf("\nSoy camarero recogi el plato del cliente y lo entrego\n");
-		
-		respuesta.type = TYPE_3; // Se entrega el plato----------------------
-		strcpy(respuesta.body, recibo.body);
-		msgsnd(queueId, &respuesta, SIZE_MSG, IPC_NOWAIT);
+		respuesta.type = recibo.type;
+		msgsnd(queue_cliente_Id,&respuesta,SIZE_MSG,IPC_NOWAIT);
+		respuesta.type =Tipo_Cola_Comida;
+		msgsnd(queue_comidas_Id,&respuesta,SIZE_MSG,IPC_NOWAIT);
 	}
 	pthread_exit(0);
 }
@@ -115,23 +152,37 @@ void * limpiarMesa(void * arg){
 	
 	tMessage recibo, respuesta;
 
-	key_t key = ftok(FILE_PATH, 2);//89
-	if (key < 0) {	report_and_exit("ftok");	}
-	int queueId = msgget(key, 0666 | IPC_CREAT);
-	if (queueId < 0) {	report_and_exit("msgget");	}
+	int queue_cliente_Id = abrirColaClientes();
 	
 	while(1){
-		msgrcv(queueId, &recibo, SIZE_MSG, TYPE_4, 0); // espero que se levante un cliente para liimpiar la mesa
+		msgrcv(queue_cliente_Id, &recibo, SIZE_MSG, Tipo_Limpiador, 0); // espero que se levante un cliente para liimpiar la mesa
 		printf("\nSoy mesero y limpio la mesa\n");
-		respuesta.type = TYPE_1; // Se habilita una mesa
-		msgsnd(queueId, &respuesta, SIZE_MSG, IPC_NOWAIT);
+		respuesta.type = Tipo_Mesas; // Se habilita una mesa
+		msgsnd(queue_cliente_Id, &respuesta, SIZE_MSG, IPC_NOWAIT);
 	}	
 	pthread_exit(0);
 }
 
-int main(int argc, char* argv[]) {
-	srand(time(NULL));
-	tMessage pedido;
+int abrirPrimeraColaClientes(){
+	key_t key = ftok(FILE_PATH, 1);
+	if (key < 0) {
+		report_and_exit("ftok");
+	}
+	int queueId = msgget(key, 0666 | IPC_CREAT);
+	if (queueId < 0) {
+		report_and_exit("msgget");
+	}
+	
+	msgctl(queueId, IPC_RMID, NULL);
+	queueId = msgget(key, 0666 | IPC_CREAT);
+	if (queueId < 0) {
+		report_and_exit("msgget");
+	}
+	
+	return queueId;
+}
+
+int abrirPrimeraColaComidas(){
 	key_t key = ftok(FILE_PATH, 2);
 	if (key < 0) {
 		report_and_exit("ftok");
@@ -146,11 +197,46 @@ int main(int argc, char* argv[]) {
 	if (queueId < 0) {
 		report_and_exit("msgget");
 	}
+	
+	return queueId;
+}
 
-	pedido.type = TYPE_1; // hay mesa
+int abrirPrimeraColaCamarero(){
+	key_t key = ftok(FILE_PATH, 3);
+	if (key < 0) {
+		report_and_exit("ftok");
+	}
+	int queueId = msgget(key, 0666 | IPC_CREAT);
+	if (queueId < 0) {
+		report_and_exit("msgget");
+	}
+	
+	msgctl(queueId, IPC_RMID, NULL);
+	queueId = msgget(key, 0666 | IPC_CREAT);
+	if (queueId < 0) {
+		report_and_exit("msgget");
+	}
+	
+	return queueId;
+}
+	
+int main(int argc, char* argv[]) {
+	srand(time(NULL));
+	tMessage pedido;
+	int queue_cliente_Id,queue_comidas_Id,queue_camarero_Id;
+	queue_cliente_Id = abrirPrimeraColaClientes();
+	queue_comidas_Id = abrirPrimeraColaComidas();
+	queue_camarero_Id = abrirPrimeraColaCamarero();
+
+	pedido.type = Tipo_Mesas; // hay mesa
 	for(int i = 0; i < M; i++){
 		//cargo 30 mesas vacias
-		msgsnd(queueId, &pedido, SIZE_MSG, IPC_NOWAIT);
+		msgsnd(queue_cliente_Id, &pedido, SIZE_MSG, IPC_NOWAIT);
+	}
+	pedido.type = Tipo_Cola_Comida; // hay espacio en la cola de comida
+	for(int i = 0; i < K; i++){
+		//cargo 10 espacios vacios en la cola de comida
+		msgsnd(queue_comidas_Id, &pedido, SIZE_MSG, IPC_NOWAIT);
 	}
 
 	pthread_t cliente[N], cocinero[C], camarero, limpiador;
@@ -158,6 +244,7 @@ int main(int argc, char* argv[]) {
 	for(int i=0; i < C; i++){
 		aux = malloc(sizeof(int));
 		*aux = i;
+		
 		pthread_create(&cocinero[i], NULL, cocinar, aux);
 	}
 
@@ -167,7 +254,7 @@ int main(int argc, char* argv[]) {
 		pthread_create(&cliente[i], NULL, comer, aux);
 	}
 	
-	pthread_create(&camarero, NULL, *entrgarComida, NULL);
+	pthread_create(&camarero, NULL, *entregarComida, NULL);
 	pthread_create(&limpiador, NULL, *limpiarMesa, NULL);
 	
 	pthread_join(camarero, NULL);

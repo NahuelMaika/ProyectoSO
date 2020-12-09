6598212671e37dde1a2e846af8120d00b260f14a
AlbaErik <erik.alba97@gmail.com>
2020-12-09 16:38:38 -0300

    Update ayuda.c

--- ayuda.c
+++ ayuda.c
@@ -3,13 +3,12 @@
 
 int main(int argc, char *argv[]){
 	printf("Los comandos posibles son: \n");
-	printf("mkdir - crea directorios \n");	
-	printf("Crear directorio mkdir 'nombre del nuevo directorio' \n");
-	printf("Eliminar un directorio rmdir 'nombre del dir a borrar' \n");
-	printf("Crear un archivo mkfil 'nombre del archivo' \n");
-	printf("Listar el contenido de un directorio showd 'nombre del dir' \n");
-	printf("Mostrar el contenido de un archivo showf 'nombre del archivo' \n");
-	printf("Mostrar una ayuda con los comando posibles ayuda \n");
-	printf("Modificar los permisos de un archivo modle 'nombre del archivo' 'nuevos permisos' \n");
+	printf("mkdir [nombre del directorio nuevo] crea directorios \n\n");	
+	printf("rmdir [nombre del directorio a borrar] Eliminar un directorio \n\n");
+	printf("mkfil [nombre del archivo] Crear un archivo \n\n");
+	printf("showd [ubicacion del directorio] Listar el contenido de un directorio. Se debe pasar junto con su ruta \n\n");
+	printf("showf [nombre del archivo] Muestra el contenido de un archivo el cual debe estar en la misma carpeta donde se ejecuta el miniShell \n o de lo contrario deberaindicar su ruta junto al nombre del archivo\n\n");
+	printf("modle [nombre del archivo] [nuevos permisos] Modifica los permisos de un archivo, se debe pasar los nuevos permisos en formato numerico\nel archivo debera estar en la misma carpeta donde se ejecuta el miniShell \n\n");
+	printf("ayuda Muestra los comando posibles \n\n");
 	return 0;
 }
